1f5086395a85b1e111c4bcda201d266279074cc0
paul.sizon <paul.sizon@outlook.com>
2024-12-13 20:36:00 -0300

    readme and metadata

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
-export const metadata = getMetadata({ title: "Scaffold-ETH 2 App", description: "Built with 🏗 Scaffold-ETH 2" });
+export const metadata = getMetadata({ title: "AI Capital", description: "crypto game with ai agent" });
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
