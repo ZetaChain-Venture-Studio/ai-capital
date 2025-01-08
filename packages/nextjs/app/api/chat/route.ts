4ddb71fea00c3f8bf10e1135640394b077a3ff50
undrcollective <support@alex.voyage>
2025-01-08 17:42:26 -0500

    backend for calling smart contract

--- packages/nextjs/app/api/chat/route.ts
+++ packages/nextjs/app/api/chat/route.ts
@@ -1,6 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 import { OpenAI } from "openai";
+import { prepareContractCall, sendAndConfirmTransaction } from "thirdweb";
+import { Account, privateKeyToAccount } from "thirdweb/wallets";
+import { tasks } from "@trigger.dev/sdk/v3";
+import { client, prizePoolSmartContract } from "@/utils/types/sdks";
+import { readContract } from "thirdweb";
+
+export const maxDuration = 300;
 
 const apiKey = process.env.OPENAI_API_KEY;
 
@@ -14,13 +21,20 @@ const openai = new OpenAI({
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const { address, userMessage } = body;
+  const { userAddress, userMessage, swapATargetTokenAddress, swapBTargetTokenAddress } = body;
 
-  if (!address || !userMessage) {
-    return NextResponse.json({ error: "Address & Prompt required" }, { status: 400 });
+  if (!userAddress || !userMessage || !swapATargetTokenAddress || !swapBTargetTokenAddress) {
+    return NextResponse.json({ error: "Address, Prompt, swap token a + b required" }, { status: 400 });
   }
 
   try {
+
+    //await whitelist(userAddress);
+
+    //Check if a user is whitelisted before handling prompt
+    const data = await checkWhitelist(userAddress);
+    console.log("data: ", data);
+
     const contextMessage = {
       role: "system",
       content: `
@@ -113,43 +127,112 @@ export async function POST(req: NextRequest) {
       `,
     };
 
-    const messages = [contextMessage, { role: "user", content: userMessage }];
+    // const messages = [contextMessage, { role: "user", content: userMessage }];
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: messages as OpenAI.Chat.Completions.ChatCompletionMessageParam[],
-    });
+    // const response = await openai.chat.completions.create({
+    //   model: "gpt-3.5-turbo",
+    //   messages: messages as OpenAI.Chat.Completions.ChatCompletionMessageParam[],
+    // });
 
-    const parsedMessage = JSON.parse(userMessage);
-    const aiResponse = JSON.parse(
-      response.choices[0].message.content ?? '{\n    "success": false,\n    "aiResponseText": "No AI response."\n}',
-    );
+    // const parsedMessage = JSON.parse(userMessage);
+    // const aiResponse = JSON.parse(
+    //   response.choices[0].message.content ?? '{\n    "success": false,\n    "aiResponseText": "No AI response."\n}',
+    // );
 
-    const result = await sql`
-      INSERT INTO messages (
-        user_address,
-        token,
-        trade_type,
-        allocation,
-        pitch,
-        ai_response_text,
-        success
-      ) VALUES (
-        ${address},
-        ${parsedMessage.token},
-        ${parsedMessage.tradeType},
-        ${parsedMessage.allocation},
-        ${parsedMessage.pitch},
-        ${aiResponse.aiResponseText},
-        ${aiResponse.success}
-      );
-    `;
+    // const result = await sql`
+    //   INSERT INTO messages (
+    //     user_address,
+    //     token,
+    //     trade_type,
+    //     allocation,
+    //     pitch,
+    //     ai_response_text,
+    //     success
+    //   ) VALUES (
+    //     ${userAddress},
+    //     ${parsedMessage.token},
+    //     ${parsedMessage.tradeType},
+    //     ${parsedMessage.allocation},
+    //     ${parsedMessage.pitch},
+    //     ${aiResponse.aiResponseText},
+    //     ${aiResponse.success}
+    //   );
+    // `;
+    // console.log("Insert successful:", result);
 
-    console.log("Insert successful:", result);
+    //Succesful prompt - transfer prize pool
+    var handle;
+    
+    if(true) {
+    //if (aiResponse.success) {
+      //To deploy a new version run:
+      //npx trigger.dev@latest deploy -e staging
+      //npx trigger.dev@latest deploy -e prod
 
-    return NextResponse.json(response.choices[0].message);
+      //TODO: This only does the asset buying and selling
+      //We also need to ensure it transfers the prize pool
+        handle = await tasks.trigger("transfer-prize-pool", {
+        userAddress: userAddress,
+        sellTargetTokenAddress: swapATargetTokenAddress,
+        buyTargetTokenAddress: swapBTargetTokenAddress,
+      });
+    }
+    //Unsuccesful prompt - dewhitelist user
+    else {
+      await deWhitelist(userAddress);
+    }
+
+    return NextResponse.json({"response-message": "lol"
+      //response.choices[0].message
+      , "handle":handle});
   } catch (error) {
-    console.error("Error calling OpenAI API:", error);
-    return NextResponse.json({ error: "Failed to call OpenAI API" }, { status: 500 });
+    console.error("Error:", error);
+    return NextResponse.json({ error: "Error" }, { status: 500 });
   }
+}
+
+const account: Account = privateKeyToAccount({
+  client,
+  privateKey: process.env.BACKEND_WALLET_PRIVATE_KEY || "",
+});
+
+async function checkWhitelist(walletAddresses: string) {
+  const data  = await readContract({
+    contract: prizePoolSmartContract,
+    method: "function whitelist(address _user)",
+    params: [walletAddresses],
+  });
+
+  return data;
+}
+
+
+async function whitelist(walletAddresses: string) {
+  const transaction = prepareContractCall({
+    contract: prizePoolSmartContract,
+    method: "function payGame(address _user)",
+    params: [walletAddresses],
+  });
+
+  const transactionReceipt = await sendAndConfirmTransaction({
+    transaction,
+    account,
+  });
+
+  console.log("Tx Receipt - whitelist: ", transactionReceipt);
+}
+
+export async function deWhitelist(walletAddresses: string) {
+  const transaction = prepareContractCall({
+    contract: prizePoolSmartContract,
+    method: "function deWhitelist(address _user)",
+    params: [walletAddresses],
+  });
+
+  const transactionReceipt = await sendAndConfirmTransaction({
+    transaction,
+    account,
+  });
+
+  console.log("Tx Receipt - deWhitelist: ", transactionReceipt);
 }
---
+++ packages/nextjs/src/trigger/prizePoolTransfer.ts
@@ -0,0 +1,60 @@
+import { logger, task } from "@trigger.dev/sdk/v3";
+import { prepareContractCall, sendAndConfirmTransaction } from "thirdweb";
+import { Account, privateKeyToAccount } from "thirdweb/wallets";
+import { deWhitelist } from "../../app/api/chat/route";
+import { client, prizePoolSmartContract } from "./../../utils/types/sdks";
+
+export const transferPrize = task({
+  id: "transfer-prize-pool",
+  // Set an optional maxDuration to prevent tasks from running indefinitely
+  maxDuration: 300, // Stop executing after 300 secs (5 mins) of compute
+  retry: {
+    maxAttempts: 1,
+  },
+  queue: {
+    concurrencyLimit: 1,
+  },
+  run: async (payload: any, { ctx }) => {
+    logger.log("Hello, world!", { payload, ctx });
+
+    logger.log("Payload!", { payload });
+
+    const userAddress: string = payload.userAddress;
+    const swapATargetTokenAddress: string = payload.sellTargetTokenAddress;
+    const swapBTargetTokenAddress: string = payload.buyTargetTokenAddress;
+
+    //await wait.for({ seconds: 5 });
+    await swapTokens(userAddress, swapATargetTokenAddress, swapBTargetTokenAddress);
+
+    return {
+      message: "Hello, world!",
+    }
+  },
+});
+
+const account: Account = privateKeyToAccount({
+  client,
+  privateKey: process.env.BACKEND_WALLET_PRIVATE_KEY || "",
+});
+
+
+async function swapTokens(
+  userAddress: string,
+  swapATargetTokenAddress: string,
+  swapBTargetTokenAddress: string,
+) {
+
+  console.log()
+  const transaction = prepareContractCall({
+    contract: prizePoolSmartContract,
+    method: "function swapTokens(address _user, address tokenA, address tokenB)",
+    params: [userAddress, swapATargetTokenAddress, swapBTargetTokenAddress],
+  });
+
+  const transactionReceipt = await sendAndConfirmTransaction({
+    transaction,
+    account,
+  });
+
+  console.log("Tx Receipt - swapTokens: ", transactionReceipt);
+}
