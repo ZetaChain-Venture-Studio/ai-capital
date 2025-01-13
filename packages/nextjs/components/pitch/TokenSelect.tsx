15607a597d6c7d18a2096bc1715ef06591b6277f
Cap <capde22@gmail.com>
2025-01-13 14:57:37 -0300

    feat: add token list on selector

--- packages/nextjs/components/pitch/TokenSelect.tsx
+++ packages/nextjs/components/pitch/TokenSelect.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { testTokens } from "../../lib/data";
+// import { testTokens } from "../../lib/data";
+import { tokens } from "~~/lib/constants";
 
 interface TokenSelectProps {
   value: string;
@@ -10,7 +11,7 @@ interface TokenSelectProps {
 export default function TokenSelect({ value, onChange }: TokenSelectProps) {
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedSymbol = e.target.value;
-    const selectedToken = testTokens.find(item => item.symbol === selectedSymbol);
+    const selectedToken = tokens.find(item => item.symbol === selectedSymbol);
     if (selectedToken) {
       onChange({ target: { value: selectedToken.address, name: "token" } } as React.ChangeEvent<HTMLInputElement>);
     }
@@ -26,15 +27,17 @@ export default function TokenSelect({ value, onChange }: TokenSelectProps) {
           id="token-select"
           name="token-select"
           onChange={handleSelectChange}
-          value={testTokens.find(token => token.address === value)?.symbol || ""}
+          value={tokens.find(token => token.address === value)?.symbol || ""}
           className="bg-white w-full rounded-md border border-gray-300 py-2 px-3 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:border-transparent"
         >
           <option value="">Select your token</option>
-          {testTokens.map(item => (
-            <option key={item.symbol} value={item.symbol}>
-              {item.token} ({item.symbol})
-            </option>
-          ))}
+          {tokens
+            .filter(item => item.symbol !== "USDC")
+            .map(item => (
+              <option key={item.symbol} value={item.symbol}>
+                {item.symbol}
+              </option>
+            ))}
         </select>
       </div>
     </div>
